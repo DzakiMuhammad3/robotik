da5976960e2ad27a828d5c6cd862812a7cdc8a4d
DzakiMuhammad3 <74474021+DzakiMuhammad3@users.noreply.github.com>
2022-12-19 16:01:24 +0700

    robotik_planar_2
    with timer pwm

--- robotik_planar/robotik_planar.ino
+++ robotik_planar/robotik_planar.ino
@@ -2,8 +2,8 @@
 #define T1_IN2 14
 #define T1_EN 13
 
-#define T2_IN1 26
-#define T2_IN2 25
+#define T2_IN1 25
+#define T2_IN2 26
 #define T2_EN 27
 
 #define T1_ENCA 34
@@ -15,8 +15,42 @@
 int T1_pulse = 0;
 int T2_pulse = 0;
 
+const int freq = 50;
+const int led_channel_1 = 0;
+const int led_channel_2 = 1;
+const int resolution = 8;
+const int PPR = 620;
+
+// Constanta for sampling time
+const double dt = 0.05;
+const double f = 1/dt;
+volatile double pulse_count = 0;
+volatile double count = 0;
+
+// Parameter for position control
+volatile double q_init = 0;
+volatile double q_final = 0;
+volatile double q_res = 0;
+volatile double q_res_old = 0;
+volatile double q_cmd = 0;
+volatile double err = 0;
+volatile double err_old = 0;
+volatile double integral_err = 0;
+volatile double duty_cycle = 0;
+
+// PID Parameter
+float kp = 5;
+float kd = 1;
+float ki = 2;
+
 void isr_T1();
 void isr_T2();
+
+void IRAM_ATTR onTimerMain();
+
+// Make timer
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
 void setup() {
   // put your setup code here, to run once:
@@ -37,25 +71,42 @@ void setup() {
   // Set pinmode fo T1 DC motor
   pinMode(T1_IN1, OUTPUT);
   pinMode(T1_IN2, OUTPUT);
-  pinMode(T1_EN, OUTPUT);
+  
 
   pinMode(T2_IN1, OUTPUT);
   pinMode(T2_IN2, OUTPUT);
-  pinMode(T2_EN, OUTPUT);
+
+  ledcSetup(led_channel_1, freq, resolution);
+  ledcAttachPin(T1_EN, led_channel_1);
+
+
+  ledcSetup(led_channel_2, freq, resolution);
+  ledcAttachPin(T2_EN, led_channel_2);
+
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimerMain, true);
+  timerAlarmWrite(timer, dt * 1000000, true);
+  timerAlarmEnable(timer);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  double deg_t1, deg_t2;
+  deg_t1 = T1_pulse/PPR*360;
+  deg_t2 = T2_pulse/PPR*360;
   digitalWrite(T1_EN, HIGH);
   digitalWrite(T2_EN, HIGH);
   digitalWrite(T1_IN1, HIGH);
   digitalWrite(T1_IN2, LOW);
   digitalWrite(T2_IN1, HIGH);
   digitalWrite(T2_IN2, LOW);
+
+  
   Serial.println("T1 Pulse: " + String(T1_pulse));
   Serial.println("T2 Pulse: " + String(T2_pulse));
-  Serial.println("T1_deg: " + String(T1_pulse/620*360));
-  Serial.println("T2_deg: " + String(T2_pulse/620*360));
+  Serial.println("T1_deg: " + String(deg_t1));
+  Serial.println("T2_deg: " + String(deg_t2));
   delay(50);
 }
 
@@ -72,4 +123,42 @@ void isr_T2(){
   else if(!digitalRead(T2_ENCA) && !digitalRead(T2_ENCB)) T2_pulse--;
   else if(digitalRead(T2_ENCA) && !digitalRead(T2_ENCB)) T2_pulse++;
   else if(!digitalRead(T2_ENCA) && digitalRead(T2_ENCB)) T2_pulse++;
+}
+
+void IRAM_ATTR onTimerMain(){
+  portENTER_CRITICAL_ISR(&timerMux);
+
+  // hitung posisi
+  q_res_old = q_res;
+  q_res = ((double)pulse_count/PPR)*360.0;
+
+  //path trajectory
+  if(count < f*3){
+    count++;
+    q_cmd = (q_final-q_init)*(double)((double)count/(double)(f*3.0)) + q_init;
+  }
+
+  err_old = err;
+  err = q_cmd - q_res;
+  integral_err += err*dt;
+  duty_cycle = kp*err + (kd/dt)*(err - err_old) + ki*integral_err;
+
+  //Output
+  if(abs(duty_cycle) > 255){
+    duty_cycle = (duty_cycle/abs(duty_cycle))*255;
+  }
+
+  if(duty_cycle > 0){
+    digitalWrite(T1_IN1, HIGH);
+    digitalWrite(T1_IN2, LOW);
+    ledcWrite(led_channel_1, abs(duty_cycle));
+  }else if(duty_cycle < 0){
+    digitalWrite(T1_IN1, LOW);
+    digitalWrite(T1_IN2, HIGH);
+    ledcWrite(led_channel_1, abs(duty_cycle));
+  }else{
+    digitalWrite(T1_IN1, HIGH);
+    digitalWrite(T1_IN2, HIGH);
+    ledcWrite(led_channel_1, 0);
+  }
 }
